fc4237654ab5cd7afd0f436e64bc5779543a7941
cuifengdan <u6589143@anu.edu.au>
2020-05-22 10:37:49 +0800

    update training

--- EmotionRecog/EmotionRecog.py
+++ EmotionRecog/EmotionRecog.py
@@ -4,13 +4,36 @@
 from FaceCNN import FaceCNN
 from FaceData import FaceData
 from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+
 
 class EmotionRecog():
 
-    def __init__(self):
-        self.cnn = FaceCNN()
+    # def __init__(self):
+    # self.cnn = FaceCNN()
 
-    def train(self):
+    def train(self, train_data, test_data, batch_size, epochs, learning_rate):
+        cnn_model = FaceCNN()
         batch_size = 20
-        train_dataset = FaceData('Faces')
-        train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+        # train_dataset = FaceData('Faces')
+        train_loader = DataLoader(train_data, batch_size, shuffle=True)
+        loss_fc = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(cnn_model.parameters(), lr=learning_rate)
+
+        for e in range(epochs):
+            loss = 0
+            cnn_model.train()
+            for imgs, labels in train_loader:
+                optimizer.zero_grad()
+                out = cnn_model.forward(imgs)
+                loss = loss_fc(out, labels)
+                loss.backward()
+                optimizer.step()
+
+            print('After {} epochs , the loss_rate is : '.format(e + 1), loss.item())
+
+
+if __name__ == '__main__':
+    print('start')
+
