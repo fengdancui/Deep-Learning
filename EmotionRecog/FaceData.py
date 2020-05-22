9ff2c865503c881a78054d3e657a05439be0618a
cuifengdan <u6589143@anu.edu.au>
2020-05-22 11:17:11 +0800

    add dir[] to store folder names

--- EmotionRecog/FaceData.py
+++ EmotionRecog/FaceData.py
@@ -24,15 +24,17 @@ class FaceData(Dataset):
         self.root = root
         self.names = []
         self.labels = []
+        self.dir = []
         for folder in os.listdir(self.root):
             path = self.root + '/' + folder
             if os.path.isdir(path):
                 for file in os.listdir(path):
                     self.names.append(file)
                     self.labels.append(self.code_dic[folder])
+                    self.dir.append(folder)
 
     def __getitem__(self, index):
-        path = self.root + '/' + self.labels[index] + '/' + self.names[index]
+        path = self.root + '/' + self.dir[index] + '/' + self.names[index]
         face = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
         face_tensor = torch.from_numpy(face)
         return face_tensor, self.labels[index]
