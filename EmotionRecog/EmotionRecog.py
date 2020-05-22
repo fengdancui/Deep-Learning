f83432f31550c5b35414c6918985df3f7047c488
cuifengdan <u6589143@anu.edu.au>
2020-05-22 09:23:29 +0800

    update conv1

--- EmotionRecog/EmotionRecog.py
+++ EmotionRecog/EmotionRecog.py
@@ -15,6 +15,17 @@ train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
 for img, label in train_loader:
     print(label)
 
+class FaceCNN(nn.Module):
+    def __init__(self):
+        super(FaceCNN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.RReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
 
 # class EmotionRecog():
-#
+
