0420c01ffaa9d7ccd04f68fd76158c810d27e831
cuifengdan <u6589143@anu.edu.au>
2020-05-22 09:44:39 +0800

    build up networks

--- EmotionRecog/EmotionRecog.py
+++ EmotionRecog/EmotionRecog.py
@@ -20,10 +20,36 @@ class FaceCNN(nn.Module):
         super(FaceCNN, self).__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=64),
-            nn.RReLU(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=256 * 6 * 6, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=4096, out_features=1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=1024, out_features=256),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=256, out_features=7),
         )
 
 
