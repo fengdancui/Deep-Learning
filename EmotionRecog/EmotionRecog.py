a32abb8e4b899f65893ce958b1d050713e5da883
cuifengdan <u6589143@anu.edu.au>
2020-05-22 11:23:32 +0800

    update main function

--- EmotionRecog/EmotionRecog.py
+++ EmotionRecog/EmotionRecog.py
@@ -3,28 +3,31 @@
 
 from FaceCNN import FaceCNN
 from FaceData import FaceData
-from torch.utils.data import DataLoader
+from torch.utils import data
 import torch.nn as nn
-import torch.optim as optim
+import torch.optim as opt
 
 
 class EmotionRecog():
 
-    # def __init__(self):
-    # self.cnn = FaceCNN()
+    def __init__(self, root, batch_size, epochs, lr):
+        all_data = FaceData(root)
+        train_size = int(0.8 * len(all_data))
+        test_size = len(all_data) - train_size
+        train_data, test_data = data.random_split(all_data, [train_size, test_size])
+        train_loader = data.DataLoader(train_data, batch_size, shuffle=True)
+        self.test_loader = data.DataLoader(test_data, batch_size, shuffle=True)
+        self.train(train_loader, epochs, lr)
 
-    def train(self, train_data, test_data, batch_size, epochs, learning_rate):
+    def train(self, train_data, epochs, lr):
         cnn_model = FaceCNN()
-        batch_size = 20
-        # train_dataset = FaceData('Faces')
-        train_loader = DataLoader(train_data, batch_size, shuffle=True)
         loss_fc = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(cnn_model.parameters(), lr=learning_rate)
+        optimizer = opt.SGD(cnn_model.parameters(), lr=lr)
 
         for e in range(epochs):
             loss = 0
             cnn_model.train()
-            for imgs, labels in train_loader:
+            for imgs, labels in train_data:
                 optimizer.zero_grad()
                 out = cnn_model.forward(imgs)
                 loss = loss_fc(out, labels)
@@ -36,4 +39,4 @@ class EmotionRecog():
 
 if __name__ == '__main__':
     print('start')
-
+    er = EmotionRecog('Faces', 10, 20, 0.05)
