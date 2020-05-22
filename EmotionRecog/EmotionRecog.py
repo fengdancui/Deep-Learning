d49d7678e9fb608eb5bbeb41c7a7afedfc96ba0a
cuifengdan <u6589143@anu.edu.au>
2020-05-22 10:09:10 +0800

    update to load data

--- EmotionRecog/EmotionRecog.py
+++ EmotionRecog/EmotionRecog.py
@@ -1,2 +1,16 @@
 # facial expression recognization
 # created on 22 May, 2020, Fengdan Cui
+
+from FaceCNN import FaceCNN
+from FaceData import FaceData
+from torch.utils.data import DataLoader
+
+class EmotionRecog():
+
+    def __init__(self):
+        self.cnn = FaceCNN()
+
+    def train(self):
+        batch_size = 20
+        train_dataset = FaceData('Faces')
+        train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
